ce92e0a65c361b1a6c13c5ae56d230114bd55d39
bdnkai <bdn.kai@gmail.com>
2023-05-07 21:28:23 -0700

    clean dubuggers

--- src/main.py
+++ src/main.py
@@ -6,19 +6,19 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['POST'])
-def process():
-    url = request.json['url']
-    usernames = get_usernames(url)
-    response = {'names': usernames}
-    return jsonify(response), 200
+# @app.route('/process', methods=['POST'])
+# def process():
+#     url = request.json['url']
+#     usernames = get_usernames(url)
+#     response = {'names': usernames}
+#     return jsonify(response), 200
 
 
 @app.route('/get_usernames', methods=['GET'])
 def get_usernames():
     url = request.args.get('url')
     results = process_image(url)
-    return jsonify(results)
+    return jsonify(results), 200
 
 
 if __name__ == '__main__':
